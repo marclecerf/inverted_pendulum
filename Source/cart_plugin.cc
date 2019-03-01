a7d69caeb75979b9e3395431c54bfe6070e20295
Marc Lecerf <mrc.lcrf@gmail.com>
2019-02-28 23:48:41 -0800

    cart_plugin.cc: stub controller + test API

--- Source/cart_plugin.cc
+++ Source/cart_plugin.cc
@@ -2,18 +2,65 @@
 #define _CART_PLUGIN_HH_
 
 #include <gazebo/gazebo.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/transport/transport.hh>
 #include <gazebo/physics/physics.hh>
 
 namespace gazebo {
-  class CartPlugin : public ModelPlugin {
-    public: CartPlugin() {}
-    public: virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
-      std::cerr << "\nCart plugin is attach to model[" <<
-        _model->GetName() << "]\n";
-    }
-  };
+class CartPlugin : public ModelPlugin {
+ public:
+  CartPlugin() {}
 
-  GZ_REGISTER_MODEL_PLUGIN(CartPlugin)
+  void SetPositionPid(ConstVector3dPtr& pid) {
+    this->pid = common::PID(pid->x(), pid->y(), pid->z());
+    this->model->GetJointController()->SetPositionPID(
+      this->joint->GetScopedName(), this->pid);
+  }
+
+  virtual void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
+    if (_model->GetJointCount() == 0) {
+      std::cerr << "Invalid joint count, cart plugin not loaded\n";
+      return;
+    }
+
+    for (const auto& j: _model->GetJoints()) {
+      std::cerr << "Joint name: '" << j->GetName() << "'\n";
+    }
+
+    this->model = _model;
+    this->joint = _model->GetJoint("InvertedPendulumCart::ChassisPendulumRevolute");
+    if (!this->joint) {
+      std::cerr << "Could not find pendulum joint!\n";
+      return;
+    }
+    this->pid = common::PID(0.5, 0, 0);
+    this->model->GetJointController()->SetPositionPID(
+      this->joint->GetScopedName(), this->pid);
+
+    this->model->GetJointController()->SetPositionTarget(
+      this->joint->GetScopedName(), 0.0);
+
+    this->node = transport::NodePtr(new transport::Node());
+    this->node->Init(this->model->GetWorld()->Name());
+    std::string topicName = "~/" + this->model->GetName() + "/kp";
+    this->sub = this->node->Subscribe(topicName,
+            &CartPlugin::OnMsg, this);
+  }
+
+ private:
+  void OnMsg(ConstVector3dPtr& _msg) {
+    this->SetPositionPid(_msg);
+  }
+
+ private:
+  physics::ModelPtr model;
+  physics::JointPtr joint;
+  common::PID pid;
+  transport::NodePtr node;
+  transport::SubscriberPtr sub;
+};
+
+GZ_REGISTER_MODEL_PLUGIN(CartPlugin)
 }  // namespace gazebo
 
 #endif  // _CART_PLUGIN_HH_
---
+++ Source/test.cc
@@ -0,0 +1,28 @@
+#include <gazebo/gazebo_config.h>
+#include <gazebo/transport/transport.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/gazebo_client.hh>
+
+int main(int _argc, char **_argv) {
+  gazebo::client::setup(_argc, _argv);
+  // Create our node for communication
+  gazebo::transport::NodePtr node(new gazebo::transport::Node());
+  node->Init();
+  // Publish to the cart topic
+  gazebo::transport::PublisherPtr pub =
+    node->Advertise<gazebo::msgs::Vector3d>("~/cart/kp");
+  // Wait for a subscriber to connect to this publisher
+  pub->WaitForConnection();
+  // Create a a vector3 message
+  gazebo::msgs::Vector3d msg;
+  // Set the velocity in the x-component
+  gazebo::msgs::Set(&msg, ignition::math::Vector3d(
+              std::atof(_argv[1]),
+              std::atof(_argv[2]),
+              std::atof(_argv[3])));
+  // Send the message
+  pub->Publish(msg);
+  // Make sure to shut everything down.
+  gazebo::client::shutdown();
+}
+
